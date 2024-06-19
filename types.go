16f45b8a945cea951c7bc454027a23c187e8d63f
Geoff Ford <geoff.golang.12@gmail.com>
2024-06-19 18:13:50 +0100

    Added IdentityID type

--- types.go
+++ types.go
@@ -30,3 +30,29 @@ func (c ConversationID) IsEmpty() bool {
 func NewConversationID() ConversationID {
 	return ident.NewID[ConversationID]()
 }
+
+// IdentityIDLength is the length of a unique identifier for an identity owning private keys
+const IdentityIDLength = 16
+
+// IdentityID represents a unique identifier for an identity that owns private keys
+type IdentityID [IdentityIDLength]byte
+
+// Copy returns a new IdentityID initialised to the same value
+func (i IdentityID) Copy() IdentityID {
+	return ident.CopyID(i)
+}
+
+// Equals returns true if the IdentityID instances are the same
+func (i IdentityID) Equals(other IdentityID) bool {
+	return ident.EqualValueID(i, other)
+}
+
+// IsEmpty returns true if the IdentityID is uninitialised
+func (i IdentityID) IsEmpty() bool {
+	return ident.EmptyID(i)
+}
+
+// NewIdentityID creates a unique, time-aligned identifier to a high level of entropy
+func NewIdentityID() IdentityID {
+	return ident.NewID[IdentityID]()
+}
